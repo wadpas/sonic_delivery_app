25458743e681e5f334cf0408b601d0cb72d7a25f
wadpas <wadpas@gmail.com>
2021-12-30 19:17:10 +0200

    category model added

---
+++ android/app/src/main/kotlin/com/example/sonic_delivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sonic_delivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Category {
+  final String? id;
+  final String? title;
+
+  const Category({
+    @required this.id,
+    @required this.title,
+  });
+}

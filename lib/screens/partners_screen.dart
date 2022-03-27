2b3fa7a559f9b00e073bf6f0f0e63fee57d34045
wadpas <wadpas@gmail.com>
2022-03-27 19:55:50 +0300

    grid update

--- lib/screens/partners_screen.dart
+++ lib/screens/partners_screen.dart
@@ -5,7 +5,7 @@ import '../screens/dishes_screen.dart';
 class PartnersScreen extends StatelessWidget {
   static const routeName = '/partners';
   const PartnersScreen({Key? key}) : super(key: key);
-  static const String routeLink = DishesScreen.routeName;
+  static const String pushRoute = DishesScreen.routeName;
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +14,9 @@ class PartnersScreen extends StatelessWidget {
         title: const Text('Partners'),
       ),
       body: const FutureGridView(
-        dbReference: routeName,
-        routeLink: routeLink,
+        collectionReference: routeName,
+        pushRoute: pushRoute,
         ratio: 7 / 2,
-        radius: 0.1,
       ),
     );
   }

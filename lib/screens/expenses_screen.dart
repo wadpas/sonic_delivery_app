3c358e7d4ee52c311672654aacecaa9a40138dfe
wadpas <wadpas@gmail.com>
2022-06-29 10:24:21 +0300

    form update

--- lib/screens/expenses_screen.dart
+++ lib/screens/expenses_screen.dart
@@ -7,7 +7,24 @@ class ExpensesScreen extends StatelessWidget {
 
   final titleContr = TextEditingController();
   final dataContr = TextEditingController();
-  final priceContr = TextEditingController();
+  final amountContr = TextEditingController();
+
+  void submitForm() {
+    final titleValue = titleContr.text;
+    final amountValue = double.parse(amountContr.text);
+
+    if (titleValue.isEmpty || amountValue <= 0) {
+      return;
+    }
+
+    FirebaseFirestore.instance
+        .collection('/users/Wq1OPs16hutxgwvObhFG/orders')
+        .add({
+      'title': titleValue,
+      'date': DateTime.now(),
+      'amount': amountValue
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,87 +46,84 @@ class ExpensesScreen extends StatelessWidget {
                     style: TextStyle(fontSize: 18)),
               );
             }
-            return Column(children: [
-              ...snapshot.data!.docs.map((e) {
-                Timestamp date = e['date'];
-                return Card(
-                  child: Container(
-                    padding: const EdgeInsets.all(7),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              e['title'],
-                              style: const TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (contex, index) {
+                  var doc = snapshot.data!.docs[index];
+                  Timestamp date = doc['date'];
+                  double amount = doc['amount'];
+                  return Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(9),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                doc['title'],
+                                style: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            Text(
-                              DateFormat.yMMMd().format(date.toDate()),
-                              style: const TextStyle(
+                              Text(
+                                DateFormat.yMMMd().format(date.toDate()),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(7),
+                            decoration: BoxDecoration(
+                              border: Border.all(
                                 color: Colors.grey,
-                                fontSize: 15,
                               ),
                             ),
-                          ],
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(7),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.grey,
+                            child: Text(
+                              amount.toStringAsFixed(2),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 19,
+                                color: Colors.red,
+                              ),
                             ),
-                          ),
-                          child: Text(
-                            '\$${e['amount']}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 19,
-                              color: Colors.red,
-                            ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-              Expanded(child: Container()),
-              Card(
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: const InputDecoration(labelText: ' Dish'),
-                      controller: titleContr,
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(labelText: ' Date'),
-                      controller: dataContr,
-                    ),
-                    TextField(
-                      decoration: const InputDecoration(labelText: ' Amount'),
-                      controller: priceContr,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        FirebaseFirestore.instance
-                            .collection('/users/Wq1OPs16hutxgwvObhFG/orders')
-                            .add({
-                          'title': titleContr.text,
-                          'date': DateTime.now(),
-                          'amount': double.parse(priceContr.text)
-                        });
-                      },
-                      child: const Text('Add'),
-                    )
-                  ],
-                ),
-              )
-            ]);
+                  );
+                });
+            // return Card(
+            //   child: Column(
+            //     children: [
+            //       TextField(
+            //         decoration: const InputDecoration(labelText: ' Dish'),
+            //         onSubmitted: (_) => submitForm(),
+            //         controller: titleContr,
+            //       ),
+            //       TextField(
+            //         decoration: const InputDecoration(labelText: ' Date'),
+            //         onSubmitted: (_) => submitForm(),
+            //         controller: dataContr,
+            //       ),
+            //       TextField(
+            //         decoration: const InputDecoration(labelText: ' Amount'),
+            //         keyboardType: TextInputType.number,
+            //         onSubmitted: (_) => submitForm(),
+            //         controller: amountContr,
+            //       ),
+            //       ElevatedButton(
+            //         onPressed: submitForm,
+            //         child: const Text('Add'),
+            //       )
+            //     ],
+            //   ),
+            // );
           },
         ));
   }
